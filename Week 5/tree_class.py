599b743abe1d4a456e7741caccce3c040433842e
repl.it user <replituser@example.com>
2020-10-28 16:56:50 +0000

    Started tree class.

---
+++ Week 5/tree_class.py
@@ -0,0 +1,33 @@
+class BinaryTree():
+  def __init__(self):
+    self.root = None
+
+  def insert(self, node):
+    if (self.root == None):
+      self.root = node
+    else:
+      self.traverseTree(self.root, node)
+      
+  def traverseTree(self, lastNode, node):
+    if (lastNode.value > node.value):
+      if (lastNode.left != None):
+        nextNode = lastNode.left
+        self.traverseTree(nextNode, node)
+      else:
+        lastNode.left = node
+    elif (lastNode.value < node.value):
+      if (lastNode.right != None):
+        nextNode = lastNode.right
+        self.traverseTree(nextNode, node)
+      else:
+        lastNode.right = node
+
+  def printTree(self):
+    print()
+
+
+class TreeNode():
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
