7ab98e2d5289faaaefc59fcf24c62bca221aea33
repl.it user <replituser@example.com>
2020-11-11 14:01:26 +0000

    Improved bubble sort by breaking from loop if no swaps were made in a pass through.

--- Week 6/bubble_sort.py
+++ Week 6/bubble_sort.py
@@ -1,11 +1,15 @@
 def bubble_sort(numList):
   for i in range(len(numList)):
-    for x in range (len(numList)-1):
+    temp = None
+    for x in range(len(numList)-1):
       if (numList[x] > numList[x+1]):
         temp = numList[x+1]
         numList[x+1] = numList[x]
         numList[x] = temp
 
+      if (x == len(numList) - 2 and temp == None):
+        return numList
+
   return numList
 
-print(bubble_sort([6,8,1,9,2,7]))
+print(bubble_sort([1,6,8,9,2,7]))
